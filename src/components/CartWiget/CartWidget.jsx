4c995d6bde0e142c183b146adf576d35fd7d260a
Maluc8 <maluc8@gmail.com>
2023-10-20 17:55:19 -0300

    Ahora el carrito en la navbar muestra la cantidad de items dentro del carrito

--- src/components/CartWiget/CartWidget.jsx
+++ src/components/CartWiget/CartWidget.jsx
@@ -1,10 +1,13 @@
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import { useContext } from 'react';
+import { cartContext } from '../../context/cartContext';
 
 const CartWidget = () => {
+  const itemsCount = useContext(cartContext).length;
   return (
     <div>
         <ShoppingCartRoundedIcon />
-        <span>(0)</span>
+        <span>({itemsCount})</span>
     </div>
   )
 }
